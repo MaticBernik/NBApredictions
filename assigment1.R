8edbfcb4fc9d47efe7396da04f595734e0d3f535
MaticBernik <mb2551@student.uni-lj.si>
2016-12-05 21:18:47 +0100

    R code: Classification: rpart,dt,nb,knn,rf,svm

--- assigment1.R
+++ assigment1.R
@@ -1,16 +1,25 @@
 ###### uvoz knjiznic ######
 #C:\Users\Robert\Documents\GitHub\NBApredictions
-#source("/home/matic/Dropbox/Inteligentni Sistemi/Assigment1/myfunctions.R") #funkcije za ocenjevanje natancnosti modela
-source("C:/Users/Robert/Documents/GitHub/NBApredictions/myfunctions.R") #funkcije za ocenjevanje natancnosti modela
+source("/home/matic/Dropbox/Inteligentni Sistemi/Assigment1/myfunctions.R") #funkcije za ocenjevanje natancnosti modela
+#source("C:/Users/Robert/Documents/GitHub/NBApredictions/myfunctions.R") #funkcije za ocenjevanje natancnosti modela
+install.packages(c("pROC","ipred", "prodlim", "CORElearn", "e1071", "randomForest", "kernlab", "nnet"))
 library(rpart)
-install.packages("CORElearn")
 library(CORElearn)
-install.packages("pROC")
+library(pROC)
+library(randomForest)
+library(e1071)
+library(nnet)
+
 
 ###### uvoz podatkov ######
-#vseIgre <- read.table("/home/matic/Dropbox/Inteligentni Sistemi/Assigment1/games.csv", header=T, sep=",")
-vseIgre <- read.table("C:/Users/Robert/Documents/GitHub/NBApredictions/games.csv", header=T, sep=",")
+vseIgre <- read.table("/home/matic/Dropbox/Inteligentni Sistemi/Assigment1/games.csv", header=T, sep=",")
+#vseIgre <- read.table("C:/Users/Robert/Documents/GitHub/NBApredictions/games.csv", header=T, sep=",")
 
+###### pregled podatkov ######
+head(vseIgre)
+summary(vseIgre)
+str(vseIgre)
+names(vseIgre)
 
 ###### izracun izhodiscne natancnosti - vecinskega klasifikatorja
 domacaZmagaIgre=vseIgre[vseIgre$HOME_win == "True",] #vse igre, kjer je zmagala domaca ekipa
@@ -54,19 +63,12 @@ ucnaMnozicaKlasifikacija=ucnaMnozica
 ucnaMnozicaKlasifikacija$finalScoreDifferential <- NULL
 testnaMnozicaKlasifikacija=testnaMnozica
 testnaMnozicaKlasifikacija$finalScoreDifferenetial <- NULL
-#od ucne mnozice locimo ciljni atribut (da ju lahko loceno podamo algoritmu za gradnjo napovednega modela)
-#ucnaMnozicaKlasifikacija_ciljni = ucnaMnozicaKlasifikacija$HOME_win 
-#ucnaMnozicaKlasifikacija_opisni = ucnaMnozicaKlasifikacija[-ucnaMnozicaKlasifikacija$HOME_win]
-#od testne mnozice locimo ciljni atribut
-#testnaMnozicaKlasifikacija_ciljni = testnaMnozicaKlasifikacija$HOME_win 
-#testnaMnozicaKlasifikacija_opisni = testnaMnozicaKlasifikacija[-testnaMnozicaKlasifikacija$HOME_win]
 
-###### model: rekurzivno particioniranje ######
-
+###### model: rekurzivno particioniranje - odlocitveno drevo ######
 model <- rpart(HOME_win ~ .,data=ucnaMnozicaKlasifikacija)
 #izris modela recursive partitioning v obliki odlocitvenega drevesa
 plot(model)
-#text(dtmpretty=0)
+text(model, pretty=0)
 #ocena natancnosti modela 
 resnicneVrednosti <- testnaMnozicaKlasifikacija$HOME_win
 napovedi <- predict(model,testnaMnozicaKlasifikacija,type="class")  #ce zelimo res odgovoriti na vprasanja iz navodil naloge namesto "class" nastavi na "prob"
@@ -74,22 +76,158 @@ t <- table(resnicneVrednosti,napovedi)
 #klasifikacijska natancnost
 sprintf("Natancnost rekurzivnega particioniranja: %f",CA(resnicneVrednosti,napovedi))
 #brier score
-sprintf("Brier score rekurzivnega particioniranja: %f",brier.score(resnicneVrednosti,napovedi))
+predMat <- predict(model, testnaMnozicaKlasifikacija, type="prob")
+obsMat <- model.matrix(~ HOME_win-1,testnaMnozicaKlasifikacija)
+sprintf("Brier score rekurzivnega particioniranja: %f",brier.score(obsMat,predMat))
 #obcutljivost
-sprintf("Obcutljivost rekurzivnega particioniranja: %f",Sensitivity(resnicneVrednosti,napovedi,HOME_win))
+sprintf("Obcutljivost rekurzivnega particioniranja: %f",Sensitivity(resnicneVrednosti,napovedi,"True"))
 #specificnost
-sprintf("Obcutljivost rekurzivnega particioniranja: %f",Specificity(resnicneVrednosti,napovedi,HOME_win))
+sprintf("Obcutljivost rekurzivnega particioniranja: %f",Specificity(resnicneVrednosti,napovedi,"True"))
 #krivulja ROC
-rocobjKlasifikacija_rpart <- (bin.resnicneVrednosti, bin.napovedi)
+rocobjKlasifikacija_rpart <- roc(resnicneVrednosti, predMat[,"True"])
+plot(rocobjKlasifikacija_rpart)
+names(rocobjKlasifikacija_rpart)
+#cutoffs <- rocobj$thresholds
+#tp = rocobj$sensitivities
+#fp = 1 - rocobj$specificities
+#dist <- (1-tp)^2 + fp^2
+#dist
+#which.min(dist)
+#best.cutoff <- cutoffs[which.min(dist)]
+# the selected cut-off value has impact on the model's performance
+#predicted.label <- factor(ifelse(predMat[,"YES"] >= best.cutoff, "YES", "NO"))
+#table(observed, predicted.label)
+#CA(bin.observed, predicted.label)
+#Sensitivity(resnicneVrednosti, predicted.label, "YES")
+#Specificity(resnicneVrednosti, predicted.label, "YES")
 
-##### model: odlocitveno drevo
+##### model: odlocitveno drevo #####
 model <- CoreModel(HOME_win ~ .,data=ucnaMnozicaKlasifikacija, model="tree")
+#ocena natancnosti modela 
+resnicneVrednosti <- testnaMnozicaKlasifikacija$HOME_win
+napovedi <- predict(model,testnaMnozicaKlasifikacija,type="class")  #ce zelimo res odgovoriti na vprasanja iz navodil naloge namesto "class" nastavi na "prob"
+t <- table(resnicneVrednosti,napovedi)
+#klasifikacijska natancnost
+sprintf("Natancnost odlocitvenega drevesa: %f",CA(resnicneVrednosti,napovedi))
+#brier score
+predMat <- predict(model, testnaMnozicaKlasifikacija, type="prob")
+obsMat <- model.matrix(~ HOME_win-1,testnaMnozicaKlasifikacija)
+sprintf("Brier score odlocitvenega drevesa: %f",brier.score(obsMat,predMat))
+#obcutljivost
+sprintf("Obcutljivost odlocitvenega drevesa: %f",Sensitivity(resnicneVrednosti,napovedi,"True"))
+#specificnost
+sprintf("Obcutljivost odlocitvenega drevesa: %f",Specificity(resnicneVrednosti,napovedi,"True"))
+#krivulja ROC
+rocobjKlasifikacija_dt <- roc(resnicneVrednosti, predMat[,"True"])
+plot(rocobjKlasifikacija_dt)
+#names(rocobjKlasifikacija_dt)
 
-##### model: naivni Bayes
+##### model: naivni Bayes #####
 model <- CoreModel(HOME_win ~ .,data=ucnaMnozicaKlasifikacija, model="bayes")
+#ocena natancnosti modela 
+resnicneVrednosti <- testnaMnozicaKlasifikacija$HOME_win
+napovedi <- predict(model,testnaMnozicaKlasifikacija,type="class")  #ce zelimo res odgovoriti na vprasanja iz navodil naloge namesto "class" nastavi na "prob"
+t <- table(resnicneVrednosti,napovedi)
+#klasifikacijska natancnost
+sprintf("Natancnost naivnega bayesa: %f",CA(resnicneVrednosti,napovedi))
+#brier score
+predMat <- predict(model, testnaMnozicaKlasifikacija, type="prob")
+obsMat <- model.matrix(~ HOME_win-1,testnaMnozicaKlasifikacija)
+sprintf("Brier score naivnega bayesa: %f",brier.score(obsMat,predMat))
+#obcutljivost
+sprintf("Obcutljivost naivnega bayesa: %f",Sensitivity(resnicneVrednosti,napovedi,"True"))
+#specificnost
+sprintf("Obcutljivost naivnega bayesa: %f",Specificity(resnicneVrednosti,napovedi,"True"))
+#krivulja ROC
+rocobjKlasifikacija_bayes <- roc(resnicneVrednosti, predMat[,"True"])
+plot(rocobjKlasifikacija_bayes)
+#names(rocobjKlasifikacija_bayes)
 
-##### model: nevronske mreze (KNN)
-model <- CoreModel(HOME_win ~ .,data=ucnaMnozicaKlasifikacija, model="knn",kInNN=5)
+##### model: nevronske mreze (KNN) - CoreModel#####
+model <- CoreModel(HOME_win ~ .,data=ucnaMnozicaKlasifikacija, model="knn",kInNN=20)
+#ocena natancnosti modela 
+resnicneVrednosti <- testnaMnozicaKlasifikacija$HOME_win
+napovedi <- predict(model,testnaMnozicaKlasifikacija,type="class")  #ce zelimo res odgovoriti na vprasanja iz navodil naloge namesto "class" nastavi na "prob"
+t <- table(resnicneVrednosti,napovedi)
+#klasifikacijska natancnost
+sprintf("Natancnost nevronske mreze (KNN): %f",CA(resnicneVrednosti,napovedi))
+#brier score
+predMat <- predict(model, testnaMnozicaKlasifikacija, type="prob")
+obsMat <- model.matrix(~ HOME_win-1,testnaMnozicaKlasifikacija)
+sprintf("Brier score nevronske mreze (KNN): %f",brier.score(obsMat,predMat))
+#obcutljivost
+sprintf("Obcutljivost nevronske mreze (KNN): %f",Sensitivity(resnicneVrednosti,napovedi,"True"))
+#specificnost
+sprintf("Obcutljivost nevronske mreze (KNN): %f",Specificity(resnicneVrednosti,napovedi,"True"))
+#krivulja ROC
+rocobjKlasifikacija_dt <- roc(resnicneVrednosti, predMat[,"True"])
+plot(rocobjKlasifikacija_dt)
+#names(rocobjKlasifikacija_dt)
+
+
+##### model: nakljucni gozdovi #####
+model <- randomForest(HOME_win ~ .,data=ucnaMnozicaKlasifikacija)
+#ocena natancnosti modela 
+resnicneVrednosti <- testnaMnozicaKlasifikacija$HOME_win
+napovedi <- predict(model,testnaMnozicaKlasifikacija,type="class")  #ce zelimo res odgovoriti na vprasanja iz navodil naloge namesto "class" nastavi na "prob"
+t <- table(resnicneVrednosti,napovedi)
+#klasifikacijska natancnost
+sprintf("Natancnost nakljucnih gozdov: %f",CA(resnicneVrednosti,napovedi))
+#brier score
+predMat <- predict(model, testnaMnozicaKlasifikacija, type="prob")
+obsMat <- model.matrix(~ HOME_win-1,testnaMnozicaKlasifikacija)
+sprintf("Brier score nakljucnih gozdov: %f",brier.score(obsMat,predMat))
+#obcutljivost
+sprintf("Obcutljivost nakljucnih gozdov: %f",Sensitivity(resnicneVrednosti,napovedi,"True"))
+#specificnost
+sprintf("Obcutljivost nakljucnih gozdov: %f",Specificity(resnicneVrednosti,napovedi,"True"))
+#krivulja ROC
+rocobjKlasifikacija_rf <- roc(resnicneVrednosti, predMat[,"True"])
+plot(rocobjKlasifikacija_rf)
+
+##### model: SVM #####
+model <- svm(HOME_win ~ .,data=ucnaMnozicaKlasifikacija)
+#ocena natancnosti modela 
+resnicneVrednosti <- testnaMnozicaKlasifikacija$HOME_win
+napovedi <- predict(model,testnaMnozicaKlasifikacija,type="class")  #ce zelimo res odgovoriti na vprasanja iz navodil naloge namesto "class" nastavi na "prob"
+t <- table(resnicneVrednosti,napovedi)
+#klasifikacijska natancnost
+sprintf("Natancnost SVM: %f",CA(resnicneVrednosti,napovedi))
+#brier score
+predMat <- predict(model, testnaMnozicaKlasifikacija, type="prob")
+obsMat <- model.matrix(~ HOME_win-1,testnaMnozicaKlasifikacija)
+sprintf("Brier score SVM: %f",brier.score(obsMat,predMat))
+#obcutljivost
+sprintf("Obcutljivost SVM: %f",Sensitivity(resnicneVrednosti,napovedi,"True"))
+#specificnost
+sprintf("Obcutljivost SVM: %f",Specificity(resnicneVrednosti,napovedi,"True"))
+#krivulja ROC
+rocobjKlasifikacija_rf <- roc(resnicneVrednosti, predMat[,"True"])
+plot(rocobjKlasifikacija_rf)
+
+##### model: nevronske mreze (KNN) - nnet #####
+#norm.data <- scale.data(rbind(ucnaMnozicaKlasifikacija,testnaMnozicaKlasifikacija))
+#norm.learn <- norm.data[1:nrow(learn),]
+#norm.test <- norm.data[-(1:nrow(learn)),]
+#model <- nnet(Class ~ ., data = norm.learn, size = 5, decay = 0.0001, maxit = 10000)
+#ocena natancnosti modela 
+#resnicneVrednosti <- testnaMnozicaKlasifikacija$HOME_win
+#napovedi <- predict(model,testnaMnozicaKlasifikacija,type="class")  #ce zelimo res odgovoriti na vprasanja iz navodil naloge namesto "class" nastavi na "prob"
+#t <- table(resnicneVrednosti,napovedi)
+#klasifikacijska natancnost
+#sprintf("Natancnost nevronske mreze (KNN) nnet: %f",CA(resnicneVrednosti,napovedi))
+#brier score
+#predMat <- predict(model, testnaMnozicaKlasifikacija, type="prob")
+#obsMat <- model.matrix(~ HOME_win-1,testnaMnozicaKlasifikacija)
+#sprintf("Brier score nevronske mreze (KNN) nnet: %f",brier.score(obsMat,predMat))
+#obcutljivost
+#sprintf("Obcutljivost nevronske mreze (KNN) nnet: %f",Sensitivity(resnicneVrednosti,napovedi,"True"))
+#specificnost
+#sprintf("Obcutljivost nevronske mreze (KNN) nnet: %f",Specificity(resnicneVrednosti,napovedi,"True"))
+#krivulja ROC
+#rocobjKlasifikacija_dt <- roc(resnicneVrednosti, predMat[,"True"])
+#plot(rocobjKlasifikacija_dt)
+#names(rocobjKlasifikacija_dt)
 
 ######!!! REGRESIJA !!!######
 #iz podatkov odstranimo atribut HOME_win, ki je sicer ciljni atribut klasifikacije
@@ -97,9 +235,3 @@ ucnaMnozicaRegresija=ucnaMnozica
 ucnaMnozicaRegresija$HOME_win <- NULL
 testnaMnozicaRegresija=testnaMnozica
 testnaMnozicaRegresija$HOME_win <- NULL
-#od ucne mnozice locimo ciljni atribut (da ju lahko loceno podamo algoritmu za gradnjo napovednega modela)
-#ucnaMnozicaRegresija_ciljni = ucnaMnozicaRegresija$finalScoreDifferential 
-#ucnaMnozicaRegresija_opisni = ucnaMnozicaRegresija[-ucnaMnozicaRegresija$finalScoreDifferential]
-#od testne mnozice locimo ciljni atribut
-#testnaMnozicaRegresija_ciljni = testnaMnozicaRegresija$finalScoreDifferential 
-#testnaMnozicaRegresija_opisni = testnaMnozicaRegresija[-testnaMnozicaRegresija$finalScoreDifferential]
